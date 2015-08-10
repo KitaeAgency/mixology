291e7422275ae923168f7c2705e6a54def956510
Adrien Goncalves <adrien.goncalves@gmail.com>
2015-08-10 17:03:28 +0200

    Add Demo File for grid
    _ Add compass for the demo
    _ Change .row class name to mixology-row
    _ Add REALLY simple HTML demo

---
+++ config.rb
@@ -0,0 +1,25 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "demo/"
+css_dir = "demo/stylesheets"
+sass_dir = "/"
+images_dir = "demo/images"
+javascripts_dir = "demo/javascripts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass partials scss && rm -rf sass && mv scss sass
